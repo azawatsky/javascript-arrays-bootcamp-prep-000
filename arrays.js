1e79b4a71bfdf9897824af6e3d677e7177820e8c
Alex Zawatsky <alexzawatsky@yahoo.com>
2019-05-28 23:15:34 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars=['snickers','hundred grand','kitkat','skittles']
 
 function addElementToBeginningOfArry(array,element) {
-  array=array.unshift(element)
+  newArray=array.unshift(element)
 }
 
 function addElementToBeginningOfArry(array,element) {
